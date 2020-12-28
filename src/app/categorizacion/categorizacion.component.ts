394dca440365b088f333d9c75ecc18437ad6ed81
nicolasbeltran <nicobeltran92@gmail.com>
2020-12-28 17:11:20 -0300

    changes

--- src/app/categorizacion/categorizacion.component.ts
+++ src/app/categorizacion/categorizacion.component.ts
@@ -12,16 +12,16 @@ export class CategorizacionComponent implements OnInit {
     {
       titleCard: 'MARCO BÁSICO "LEY 24901"',
       description:
-        'Somos expertos en el instrumento de evaluacion NECESARIO para CATEGORIZAR y ACREDITAR los servicios de atención a las personas con discapacidad y a los efectos de su incorporación al Registro Nacional de Prestadores de Servicios de Atención de Personas con Discapacidad',
+        'Somos expertos en el instrumento de evaluación NECESARIO para CATEGORIZAR y ACREDITAR los servicios de atención a las personas con discapacidad y a los efectos de su incorporación al Registro Nacional de Prestadores de Servicios de Atención de Personas con Discapacidad',
       modalityDescription: [
-        'Estimulación temprana',
+        'Estimulación Temprana',
         'Educación Inicial',
         'Educación General Básica',
-        'Servicio de integración en escuela común',
+        'Servicio de Integración en escuela común',
         'Formación Laboral',
         'Centro de Día',
         'Centro Educativo Terapéutico',
-        'Centro de rehabilitación para personas con dicapacidad visual',
+        'Centro de Rehabilitación para personas con dicapacidad visual',
         'Hogar',
       ],
       icon: './assets/05.png',
@@ -33,7 +33,7 @@ export class CategorizacionComponent implements OnInit {
         'Somos expertos en el instrumento de evaluación NECESARIO para la categorización de establecimientos de rehabilitación.',
       modalityDescription: ['Centros, Clínicas, Sanatorios, Institutos'],
       icon: './assets/06.png',
-      id: 2 
+      id: 2
     },
   ];
   ngOnInit(): void {}
