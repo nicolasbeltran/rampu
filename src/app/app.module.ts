db80e88d64d104a714afffb0c5e308072284692b
nicolasbeltran <nicobeltran92@gmail.com>
2021-01-09 18:16:13 -0300

    Adds new icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { NuestroEquipoComponent } from './sobre-nosotros/nuestro-equipo/nuestro-
 import { ContactarComponent } from './sobre-nosotros/contactar/contactar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainSobreNosotrosComponent } from './sobre-nosotros/main-sobre-nosotros/main-sobre-nosotros.component';
-// import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 @NgModule({
   declarations: [
@@ -49,7 +49,7 @@ import { MainSobreNosotrosComponent } from './sobre-nosotros/main-sobre-nosotros
     NgxSpinnerModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    // ScrollToModule.forRoot(),
+    ScrollToModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-// import { ScrollToConfigOptions, ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
+import { ScrollToConfigOptions, ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
 
 @Component({
   selector: 'nav-bar',
@@ -8,21 +8,21 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  // constructor(
-  //   private router: Router,
-  //   private scrollToService: ScrollToService
-  // ) {
-  //   this.router.events.subscribe((event) => {
-  //     if (event instanceof NavigationEnd) {
-  //       const config: ScrollToConfigOptions = {
-  //         target: event.urlAfterRedirects.replace('/', '#'),
-  //       };
-  //       setTimeout(() => {
-  //         this.scrollToService.scrollTo(config);
-  //       }, 100);
-  //     }
-  //   });
-  // }
+  constructor(
+    private router: Router,
+    private scrollToService: ScrollToService
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const config: ScrollToConfigOptions = {
+          target: event.urlAfterRedirects.replace('/', '#'),
+        };
+        setTimeout(() => {
+          this.scrollToService.scrollTo(config);
+        }, 100);
+      }
+    });
+  }
 
   ngOnInit(): void {}
 }
